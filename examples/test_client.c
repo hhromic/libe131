e7305ff84d4850245f1adf1b7d8fe3f896073075
Hugo Hromic <hhromic@gmail.com>
2016-08-02 23:55:39 +0100

    improve interval and use multiple of 3 channels

--- examples/test_client.c
+++ examples/test_client.c
@@ -14,8 +14,8 @@ int main() {
   if ((sockfd = e131_socket())<0)
     err(EXIT_FAILURE, "e131_socket");
 
-  // initialize a new E1.31 packet in universe 1 with 20 channels
-  e131_pkt_init(1, 20, &packet);
+  // initialize a new E1.31 packet in universe 1 with 24 channels
+  e131_pkt_init(1, 24, &packet);
   memcpy(&packet.frame.source_name, "E1.31 Test Client", 18);
 
   // set remote system address as unicast
@@ -25,13 +25,13 @@ int main() {
   // loop to send cycling levels for each channel
   uint8_t level = 0;
   for (;;) {
-    for (size_t pos=0; pos<20; pos++)
+    for (size_t pos=0; pos<24; pos++)
       packet.dmp.property_values[pos + 1] = level;
     level++;
     if (e131_send(sockfd, &packet, &dest)<0)
       err(EXIT_FAILURE, "e131_send");
     e131_pkt_dump(&packet);
     packet.frame.sequence_number++;
-    sleep(1);
+    usleep(250000);
   }
 }
