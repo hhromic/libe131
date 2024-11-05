55c2ee33fd15f80cd94f8b7507e3e6aaefc58d98
Dmitry Klepikov <dmitry@pixoutserver.com>
2024-11-05 18:56:47 +0200

    Examples CMake
    Examples could be build or omitted based on Cmake configuration.

---
+++ examples/test_mcast_client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <err.h>
+#include <e131.h>
+
+int main() {
+  int sockfd = 0;
+  e131_packet_t packet;
+  e131_addr_t dest;
+  char buf[100];
+
+  // create a socket for E1.31
+  if ((sockfd = e131_socket()) < 0)
+    err(EXIT_FAILURE, "e131_socket");
+
+  // configure socket to use the default network interface for outgoing multicast data
+  if (e131_multicast_iface(sockfd, 0) < 0)
+    err(EXIT_FAILURE, "e131_multicast_iface");
+
+  // initialize the new E1.31 packet in universe 1 with 24 slots in preview mode
+  e131_pkt_init(&packet, 1, 24);
+  memcpy(&packet.frame.source_name, "E1.31 Test Client", 18);
+
+  // set remote system destination as unicast address
+  if (e131_multicast_dest(&dest, 1, E131_DEFAULT_PORT) < 0)
+    err(EXIT_FAILURE, "e131_unicast_dest 1");
+
+  if( e131_dest_str(buf, &dest) < 0)
+    err(EXIT_FAILURE, "e131_unicast_dest 2");
+
+  fprintf(stderr, "Sending to: %s\n", buf);
+
+  // loop to send cycling levels for each slot
+  uint8_t level = 0;
+  for (;;) {
+    for (size_t pos=0; pos<24; pos++)
+      packet.dmp.prop_val[pos + 1] = level;
+
+    level++;
+    if (e131_send(sockfd, &packet, &dest) < 0)
+      err(EXIT_FAILURE, "e131_send");
+
+    e131_pkt_dump(stderr, &packet);
+    packet.frame.seq_number++;
+    usleep(250000);
+  }
+}
